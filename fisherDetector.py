3bd74c9b8b4ceb41fe3f8c8cc0b5023700ad1b95
luoyoul <yoyo_luo@sfu.ca>
2018-07-24 22:30:10 -0700

    Beautify code, add comment, add support for steam64id, add debug option

--- fisherDetector.py
+++ fisherDetector.py
@@ -1,57 +1,86 @@
+import argparse
 import urllib.request
 import json
 
-OPENDOTA = "https://api.opendota.com/api"
-#################
-# YOUR API HERE #
-#################
-APIKEY = ""
+OPENDOTA = 'https://api.opendota.com/api'
 CODING = None
+steam64id0 = 76561197960265728
+
+debugModeOn = False
 
 # Open a URL, decode it as UTF-8 and load it into list
 def openLoad(url):
 	contents = urllib.request.urlopen(url).read()
-	# contents = contents.decode('utf8').replace("'", '"')
 	return json.loads(contents)
 
-def playerMatches(steamid):
-	return OPENDOTA + "/players/" + str(steamid) + "/recentMatches?api_key=" + APIKEY
+# Convert a steam64id to steam32id
+def getPlayer32ID(steam64id):
+	return str(int(steam64id) - steam64id0)
 
-def matchDetail(matchid):
-	return OPENDOTA + "/matches/" + str(matchid) + "?api_key=" + APIKEY
+# Returns a link of a players recent 20 matches
+def getPlayerMatches(steamid):
+	return openLoad(OPENDOTA + '/players/' + str(steamid) \
+		+ '/recentMatches?api_key=' + APIKEY)
+
+# Returns a link of a match's detail
+def getMatchDetail(matchid):
+	return openLoad(OPENDOTA + '/matches/' + str(matchid) \
+		+ '?api_key=' + APIKEY)
+
+def printMatchInfo(match):
+	with open(str(match['match_id']) + '.txt', 'w') as f:
+		f.write(json.dumps(match))
+	print('Printing match detail for match {}'.format(match['match_id']))
+	print('Score {}:{}'.format(match['radiant_score'], match['dire_score']))
+	print('barracks_status:{:06b}:{:06b}'.format( \
+		match['barracks_status_radiant'], match['barracks_status_dire']) )
+	print('tower_status:{:015b}:{:015b}'.format( \
+		match['tower_status_radiant'], match['tower_status_dire']))
+	print('\n')
 
 
-def analyze(user, detail):
+# Analyze if a player in a won match wins overwhelmingly
+def analyze(user, match):
+	# Initialization
+	acceptedGameMode = ['1','2', '3', '4', '16','22']
+	acceptedLobbyType = ['0','2','5','6','7','9']
 	user = str(user)
 	ans = {
-		'win' : False, 
 		'isATrap' : False, 
 		'isATriumph' : False 
 	}
-	for player in detail['players']:
+	# Check if match is valid for testing
+	for player in match['players']:
 		if (str(player['account_id']) != user):
 			continue
 		break
-	if (str(player['account_id']) != user) or \
-			(str(detail['lobby_type']) not in ['0','2','5','6','7','9']):
+	if (str(player['account_id']) != user) \
+			or (str(match['game_mode']) not in acceptedGameMode) \
+			or (str(match['lobby_type']) not in acceptedLobbyType):
 		ans['dataInvalid'] = True
 		return ans
-	ans['win'] = detail['radiant_win'] == player['isRadiant']
+	# Check if the user wins
+	ans['win'] = match['radiant_win'] == player['isRadiant']
 	if (not ans['win']):
 		return ans
-	if detail['radiant_win']:
+	# Record the scores of winner and loser
+	if match['radiant_win']:
 		winner = 'radiant'
 		loser = 'dire'
 	else:
 		winner = 'dire'
 		loser = 'radiant'
-	winnerScore = int(detail[winner + '_score'])
-	loserScore = int(detail[loser + '_score'])
+	winnerScore = int(match[winner + '_score'])
+	loserScore = int(match[loser + '_score'])
+	# Test if the ratio is high and the sum of scores is high
 	winLoseRatio = winnerScore/loserScore
 	if winLoseRatio >= 2:
 		if winnerScore/loserScore >= 3.2:
 			if winnerScore + loserScore >= 50:
 				ans['isATrap'] = True
+				if debugModeOn:
+					print('It seems like a trap:')
+					printMatchInfo(match)
 			elif winnerScore + loserScore >= 40:
 				ans['isATriumph'] = True
 		else:
@@ -59,11 +88,10 @@ def analyze(user, detail):
 				ans['isATriumph'] = True
 	return ans
 
-
-
-
-
-
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--debug', action='store_true', default=False)
+args = parser.parse_args()
+debugModeOn = args.debug
 
 
 with open('users.txt', 'r') as f:
@@ -72,19 +100,32 @@ with open('users.txt', 'r') as f:
 	users = list(filter(None, users))
 
 if (len(users) < 5):
-	print("The number of users is less than 5")
-	input("Press Enter to continue")
+	print('The number of users is less than 5')
+	input('Press Enter to continue')
+
+users = [ getPlayer32ID(x) if int(x) > steam64id0 else x for x in users]
+
+print(users)
+
+
 
 for user in users:
+	print('**************** For player {} ****************'.format(user))
 	trap = 0
 	triumph = 0
 
 	win = 0
 	lose = 0
 
-	matches = openLoad(playerMatches(user))
+	matches = getPlayerMatches(user)
 	for match in matches:
-		detail = openLoad(matchDetail(match['match_id']))
+		userInRadiant = match['player_slot'] < 128
+		userWin = userInRadiant == match['radiant_win']
+		if not userWin:
+			lose += 1
+			continue
+		win += 1
+		detail = getMatchDetail(match['match_id'])
 		report = analyze(user, detail)
 		if 'dataInvalid' in report.keys():
 			continue
@@ -107,12 +148,10 @@ for user in users:
 		else:
 			personality = 'a tiny little fish'
 
-	print("Player " + str(user) + ": " + personality "\n")
-	print("Since totalGames:" + str(totalGames) + " triumph:" + \
-			str(triumph) + " trap:" + str(trap) + "\n")
-
-
-
-
-
+	print('Player ' + str(user) + ': ' + personality)
+	if totalGames > 0:
+		print('Since totalGames:' + str(totalGames) + ' triumph:' + \
+			str(triumph) + ' trap:' + str(trap) + ' winratio:' + \
+			str(round(win/totalGames,2)*100) + '%')
+	print('\n')
 
